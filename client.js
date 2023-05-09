33a1380e536918942c50a33fc639207b0593dc96
Matthew Dacre <matthewd@bbd.co.za>
2023-05-09 15:38:33 +0200

    Basic game creation

--- client.js
+++ client.js
@@ -1,14 +1,11 @@
-function createConnection() {
-    const socket = new WebSocket("ws://localhost:8080");
+const socket = new WebSocket("ws://localhost:8080")
 
-    socket.onopen = function(e) {
-        console.log("[open] Connection established");
-        console.log("Sending to server");
-        socket.send("My name is John");
-    };
-
-    socket.onmessage = function(event) {
-        console.log(`[message] Data received from server: ${event.data}`);
-    };
+function createGame() {
+    socket.send(JSON.stringify({
+        numberOfQuestions: 1,
+        requestType: "CREATE"
+    }));
 }
 
+function joinGame() {
+}
--- server.js
+++ server.js
@@ -1,11 +1,68 @@
-const {WebSocketServer} = require("ws"); 
+import { getQuestions } from "./questions.js";
+import { WebSocketServer } from "ws"; 
 
 const wss = new WebSocketServer({ port: 8080 });
 
-wss.on("connection", (ws) => {
-  ws.on('message', function message(data) {
-    console.log('received: %s', data);
-  });
+const liveGames = [];
 
-  ws.send('something');
+function parseMessage(msg, ws) {
+  const getRandomCode = () => Math.random().toString(36).slice(2, 7).toUpperCase();
+  console.log(`Received Message: ${JSON.stringify(msg)}`);
+  switch(msg['requestType']) {
+    case "CREATE":
+      let gameID = getRandomCode();
+      liveGames.push({
+        playerProfile: {},
+        gameID: gameID,
+        numberOfQuestions: msg['numberOfQuestions'],
+        currentQuestion: 0,
+        numAnswers: 0,
+        started: false,
+        questions: getQuestions()
+      });
+      ws.send(`gameID: ${gameID}`);
+      console.log(liveGames);
+      break;
+    default:
+      return;
+  }
+
+}
+
+wss.on("connection", (ws) => {
+  ws.on("message", (msg) => parseMessage(JSON.parse(msg), ws));
 });
+
+/**
+ * {
+ *    RequestType: "JOIN", "CREATE", "ANSWER"
+ * }
+ */
+
+/*
+
+  game = {
+    players = [players],
+    maxQuestions = int,
+    currentQuestion = int,
+    numAnswers = int,
+    questions = [question objects]
+  }
+
+  player = {
+    socket,
+    uuid,
+    score
+  }
+
+*/
+
+// Creating rooms //
+
+function receiveAnswers() {
+  /*
+
+  Once all answers recieved for a room in session, send answer then new question to all clients
+
+  */
+}
