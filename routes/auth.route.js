3aa1c277877cdef6ac114ab4c6604879a59107e1
Johan Smit <johansm@bbd.co.za>
2023-05-12 16:01:30 +0200

    Reset with this

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,1 +1,66 @@
-Reece is a little ..
+import passport from "passport";
+import path from "path";
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+import { Router } from "express";
+import authController from "../controllers/auth.controller.js";
+import passportGoogle from "../services/passport/passport-google.js";
+import catchAsync from "../middleware/catchAsync.js";
+import authentication from '../middleware/authenticate.js';
+
+const { protectedRoute, socialAuth } = authController;
+
+const authRouter = Router()
+
+/* GET /login
+ *
+ * This route prompts the user to log in.
+ *
+ * The 'login' view renders an HTML page, which contain a button prompting the
+ * user to sign in with Google.  When the user clicks this button, a request
+ * will be sent to the `GET /login/federated/accounts.google.com` route.
+ */
+authRouter.get('/login', function(req, res, next) {
+  res.sendFile(__dirname + '/login.html');
+});
+
+/* GET /login/federated/accounts.google.com
+ *
+ * This route redirects the user to Google, where they will authenticate.
+ *
+ * Signing in with Google is implemented using OAuth 2.0.  This route initiates
+ * an OAuth 2.0 flow by redirecting the user to Google's identity server at
+ * 'https://accounts.google.com'.  Once there, Google will authenticate the user
+ * and obtain their consent to release identity information to this app.
+ *
+ * Once Google has completed their interaction with the user, the user will be
+ * redirected back to the app at `GET /oauth2/redirect/accounts.google.com`.
+ */
+authRouter.get('/login/federated/google', passport.authenticate('google'));
+
+/*
+    This route completes the authentication sequence when Google redirects the
+    user back to the application.  When a new user signs in, a user account is
+    automatically created and their Google account is linked.  When an existing
+    user returns, they are signed in to their linked account.
+*/
+authRouter.get('/oauth2/redirect/google', passport.authenticate('google', {
+  successReturnToOrRedirect: '/',
+  failureRedirect: __dirname + '/public//login.html'
+}));
+
+/* POST /logout
+ *
+ * This route logs the user out.
+ */
+authRouter.post('/logout', function(req, res, next) {
+  req.logout(function(err) {
+    if (err) { return next(err); }
+    res.redirect('/');
+  });
+});
+
+export default authRouter;
