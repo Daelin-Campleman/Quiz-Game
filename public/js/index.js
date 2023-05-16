4b819232072b16ddd78b15db66ad28500dcc86f4
Daelin Campleman <daelinc@gmail.com>
2023-05-16 15:03:56 +0200

    added sign in button to welcome page
    Added the sign on buttons to the main welcome page to reduce the number of steps the user has to take.

--- public/js/index.js
+++ public/js/index.js
@@ -10,11 +10,9 @@ if (window.location.host.includes("-qa")) {
 
 document.getElementById("name-heading").innerHTML = "Welcome " + await fetchName()
 
-document.getElementById("start-btn").addEventListener("click", function () {
-    document.getElementById("error-message").textContent = "Doing fake AD stuff...";
-    document.getElementById("start-btn").disabled = true;
-    // 2 second delay
-    setTimeout(function () {
-        window.location = "/home";
-    }, 2000);
+document.getElementById("start-btn").addEventListener("click", function (event) {
+    let provider = event.currentTarget.dataset.provider;
+
+    window.location = `/auth/login/federated/${provider}`;
+
 });
