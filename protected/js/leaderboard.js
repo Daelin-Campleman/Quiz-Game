6abe0045d8a7d49d1fc1095c337448e2d5224f7a
reece-stephenson <reeces@bbd.co.za>
2023-05-18 21:40:32 +0200

    Leaderboard in nice state

---
+++ protected/js/leaderboard.js
@@ -0,0 +1,5 @@
+window.onload = function() {
+    document.getElementById("first").className += "loaded-first";
+    document.getElementById("second").className += "loaded-second";
+    document.getElementById("third").className += "loaded-third";
+}
