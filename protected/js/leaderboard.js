e34149749937ed33c27565a5e849449a3d51fdab
reece-stephenson <reeces@bbd.co.za>
2023-05-19 22:00:19 +0200

    Make table and podium dynamic

--- protected/js/leaderboard.js
+++ protected/js/leaderboard.js
@@ -3,3 +3,40 @@ window.onload = function() {
     document.getElementById("second").className += "loaded-second";
     document.getElementById("third").className += "loaded-third";
 }
+
+const res = [
+    { name: "Bob Stone", points: 15 },
+    { name: "John Wick", points: 20 },
+    { name: "John B", points: 5 },
+    { name: "Logan Wick", points: 10 },
+    { name: "Bob John", points: 5 },
+]
+
+function loadLeaderBoard(items) {
+    items.sort((a, b) => b["points"] - a["points"]);
+
+    const table = document.getElementById("results");
+    const first = document.getElementById("first");
+    const second = document.getElementById("second");
+    const third = document.getElementById("third");
+    
+    first.innerHTML += items[0].name;
+    second.innerHTML += items[1].name;
+    third.innerHTML += items[2].name;    
+
+    for (let i = 0; i < items.length; i++) {
+        let row = table.insertRow();
+        let number = row.insertCell(0);
+        let name = row.insertCell(1);
+        let points = row.insertCell(2);
+
+        name.className = "name";
+        points.className = "points";
+        number.className = "number";
+
+        name.innerHTML = items[i].name;
+        points.innerHTML = items[i].points;
+        number.innerHTML = i+1;
+    }
+}
+loadLeaderBoard(res);
