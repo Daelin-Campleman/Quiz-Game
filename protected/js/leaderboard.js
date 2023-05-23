3f35676f34759c3b5985c8c0b5a736b656073f7f
reece-stephenson <reeces@bbd.co.za>
2023-05-23 22:37:40 +0200

    Update order when ties occur

--- protected/js/leaderboard.js
+++ protected/js/leaderboard.js
@@ -1,47 +1,11 @@
-window.onload = function() {
-    document.getElementById("first").className += "loaded-first";
-    document.getElementById("second").className += "loaded-second";
-    document.getElementById("third").className += "loaded-third";
-}
-
 const res = [
-    { name: "Bob Stone", points: 15 },
-    { name: "John Wickernoodle", points: 20 },
+    { name: "Bob Stone", points: 30 },
+    { name: "John Wickernoodle", points: 30 },
     { name: "John B", points: 5 },
     { name: "Logan Wickernoodle", points: 10 },
     { name: "Bobby pins", points: 5 },
     { name: "Chris P bacon", points: 30 },
 ]
-
-function loadLeaderBoard(items) {
-    items.sort((a, b) => b["points"] - a["points"]);
-
-    const table = document.getElementById("results");
-    const first = document.getElementById("first");
-    const second = document.getElementById("second");
-    const third = document.getElementById("third");
-    
-    first.innerHTML += items[0].name;
-    second.innerHTML += items[1].name;
-    third.innerHTML += items[2].name;    
-
-    for (let i = 0; i < items.length; i++) {
-        let row = table.insertRow();
-        let number = row.insertCell(0);
-        let name = row.insertCell(1);
-        let points = row.insertCell(2);
-
-        name.className = "name";
-        points.className = "points";
-        number.className = "number";
-
-        name.innerHTML = items[i].name;
-        points.innerHTML = items[i].points;
-        number.innerHTML = i+1;
-    }
-}
-loadLeaderBoard(res);
-fetchGame();
 
 async function fetchGame() {
     let response = await fetch("/game/leaderboard");
@@ -52,3 +16,61 @@ async function fetchGame() {
     // return data.user.name;
 }
 
+function loadLeaderBoard(items) {
+    items.sort((a, b) => b["points"] - a["points"]);
+
+    const table = document.getElementById("results");   
+
+    let order = 0;
+    
+    // Loop through
+    for (let i = 0; i < items.length; i++) {
+        if (i != 0 && items[i].points != items[i - 1]?.points) {
+            loadPodium(items[i].name, order+1, i + 1);
+            order = i;
+        }
+        else {
+            loadPodium(items[i].name, order, i + 1);
+        }       
+        
+        let row = table.insertRow();
+        let number = row.insertCell(0);
+        let name = row.insertCell(1);
+        let points = row.insertCell(2);
+        
+        name.className = "name";
+        points.className = "points";
+        number.className = "number";
+        
+        name.innerHTML = items[i].name;
+        points.innerHTML = items[i].points;
+        number.innerHTML = order+1;
+    }
+}
+
+function loadPodium(name, result, position) {
+    if (position > 3) {
+        return;
+    }
+
+    const podium = document.getElementById(`podium${position}`);
+    
+    switch (result) {
+        case 0:
+            podium.className += "loaded-gold";
+            break; 
+
+        case 1:
+            podium.className += "loaded-silver";
+            break;
+        
+        case 2:
+            podium.className += "loaded-bronze";
+            break;
+    }
+
+    podium.innerHTML += name;
+}
+
+loadLeaderBoard(res);
+fetchGame();
